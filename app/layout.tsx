611668edb1d2bd7e8e32cafcca42a1e41d3ec390
Clemens Zirbes <70413337+ProfessorOwl@users.noreply.github.com>
2026-03-03 23:32:59 +0100

    Vercel Analytics und SpeedInsights zugefügt

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,8 @@ import localFont from "next/font/local";
 import { LayoutGrid } from "./components/LayoutGrid";
 import "./styles/globals.css";
 import "katex/dist/katex.min.css";
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata = {
     title: "zuzule",
