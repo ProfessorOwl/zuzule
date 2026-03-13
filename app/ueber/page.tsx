0c3a568161da0fbdfa6e74a66b5d1a7f79d384b0
Clemens Zirbes <70413337+ProfessorOwl@users.noreply.github.com>
2026-03-13 14:01:43 +0100

    Feat: add videos for instructions on how to use the site

--- app/ueber/page.tsx
+++ app/ueber/page.tsx
@@ -1,4 +1,4 @@
-import { Space, Title } from "@mantine/core";
+import { Box, Divider, Flex, SimpleGrid, Space, Title } from "@mantine/core";
 import { Metadata } from "next";
 import { Fragment } from "react/jsx-runtime";
 
@@ -11,11 +11,28 @@ export default function Ueber() {
         <Title order={1} mb={"md"} mt={"xl"}>Über das Projekt</Title>
        „Zurück zum Lehrer“ oder auch <b>zuzule</b> ist eine Lernwebsite, die es ermöglicht, Online-Lerninhalte gezielt im Unterricht einsetzen zu können. Sie wird im Rahmen einer Masterarbeit gestaltet und beinhaltet im Moment Geometrie und Körper als Themen.
         <Title order={2} mb={"md"} mt={"xl"}>Motivation</Title>
-        Die meisten Lernwebsiten sind übersät mit unnötigen Inhalten wie Werbung, Anmeldeaufforderungen, unübersichtliche Inhaltsvorschläge, etc. Die Lerninhalte sind oft nach keinen Kriterien im Lehrplan gestaltet – dadurch benötigen sie entweder zu viel Vorwissen oder gehen nicht tief genug in die Thematik herein. Ein letzter Punkt ist die fehlende Kontrolle über die Inhalte. Der Lerngruppe kann entweder die ganze Website oder nichts geteilt werden. Lösungen, unnötige oder ablenkende INhalte können nicht ausgeblendet werden.
+        Die meisten Lernwebsiten sind übersät mit unnötigen Inhalten wie Werbung, Anmeldeaufforderungen, unübersichtliche Inhaltsvorschläge, etc. Die Lerninhalte sind oft nach keinen Kriterien im Lehrplan gestaltet – dadurch benötigen sie entweder zu viel Vorwissen oder gehen nicht tief genug in die Thematik herein. Ein letzter Punkt ist die fehlende Kontrolle über die Inhalte. Der Lerngruppe kann entweder die ganze Website oder nichts geteilt werden. Lösungen, unnötige oder ablenkende Inhalte können nicht ausgeblendet werden.
         <Space/>
         <b>zuzuLe</b> versucht, all diese Schwächen zu kompensieren.
 
         <Title order={2} mb={"md"} mt={"xl"}>Funktionsweise</Title>
-        Jedes Kapitel lässt sich mit dem Kästchen neben dem Titel aus- und ausklappen. Diese Änderung wird auch in der Kapitelübersicht rechts reflektiert. Durch Klicken auf den Button „Inhalte freigeben“ oben rechts werden die gemachten Änderungen festgehalten und eingeklappte Kapitel vollständig ausgeblendet. Der Link dieser Seite kann nun an die Lerngruppe weitergegeben werden, sodass eine personalisierte Websiteerfahrung gewährleistet werden kann.
+        <SimpleGrid cols={{base: 1, md: 2}}>
+            <Box>Jedes Kapitel lässt sich mit dem Kästchen neben dem Titel ein- und ausklappen. Diese Änderung wird auch in der Kapitelübersicht rechts reflektiert. </Box>
+            <video width={"100%"} autoPlay loop>
+                <source src="/Anleitung1.webm" type="video/webm" />
+            </video>
+
+        </SimpleGrid>
+
+        <Divider/>
+        <SimpleGrid cols={{base: 1, md: 2}}>
+        <Box>
+        Durch Klicken auf den Button „Inhalt freigeben“ oben rechts werden die gemachten Änderungen festgehalten und eingeklappte Kapitel vollständig ausgeblendet. Der Link dieser Seite kann nun an die Lerngruppe weitergegeben werden, sodass eine personalisierte Websiteerfahrung gewährleistet werden kann.
+        </Box>
+            <video width={"100%"} autoPlay loop>
+                <source src="/Anleitung2.webm" type="video/webm" />
+            </video>
+        </SimpleGrid>
+
         </Fragment>;
 }
